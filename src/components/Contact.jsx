1ba54b47af946fbdf6fa56c04bc3ff148b9b0bf8
Pooja Patel <poojaclass1994@gmail.com>
2026-02-27 21:55:44 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,17 +3,28 @@ import React, { useState } from 'react'
 const Contact = () => {
     const [message, setMessage] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        setMessage("Thanks! Message sent.");
+        const formData = new FormData(e.target);
 
-        setTimeout(() => {
-            setMessage("");
-        }, 4000);
+        try {
+            await fetch("https://formsubmit.co/poojapatel230494@gmail.com", {
+                method: "POST",
+                body: formData
+            });
 
-        e.target.reset();
-    }
+            setMessage("Thanks! Message sent.");
+            e.target.reset();
+
+            setTimeout(() => {
+                setMessage("");
+            }, 4000);
+
+        } catch (error) {
+            setMessage("Something went wrong.");
+        }
+    };
     return (
         <>
             {/* CONTACT Section Start */}
@@ -45,7 +56,7 @@ text-neutral-900 dark:text-white">
 bg-neutral-100 dark:bg-neutral-900
 border border-neutral-200 dark:border-neutral-800
 shadow-md hover:shadow-lg transition">
-                            <form id="contactForm" action="https://formsubmit.co/poojapatel230494@gmail.com" method="post" className="mt-6 grid gap-4" onSubmit={handleSubmit}>
+                            <form id="contactForm" className="mt-6 grid gap-4" onSubmit={handleSubmit}>
                                 {/* Name & Email */}
                                 <div className="grid md:grid-cols-2 gap-4">
                                     <input name="name" aria-label="Your name" placeholder="Your name" required className="p-3 rounded-lg 
